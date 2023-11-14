0fa6afcba77e5403fd746e8aca531c3abf275f2f
munim <munimali250@gmail.com>
2023-11-14 10:30:16 -0500

    Trees done a bit

--- src/Tree.java
+++ src/Tree.java
@@ -1,3 +1,11 @@
 public class Tree {
-    public Tree() {}
+    private char treeSize;
+    // constructor for the Tree Class
+    public Tree(char treeSize) {
+        this.treeSize = treeSize;
+    }
+
+    public char getTreeSize() {
+        return treeSize;
+    }
 }
