69cd41ba56bd4703144225009b6d9e369f9ba91a
owner <munimali250@gmail.com>
2023-11-14 20:55:54 -0500

    Worked a bit on a few new methhods, newWeek(), mainMenu() and start(), as well as eddited the game construtor a little and declaring and initializing more variables in the Game class

--- src/GameIO.java
+++ src/GameIO.java
@@ -2,7 +2,25 @@ import java.util.Scanner;
 
 public class GameIO {
     private Scanner scan = new Scanner(System.in);
-    public GameIO() {}
+    private Game game;
+    public GameIO() {
+        game = new Game();
+    }
+
+    public void start() {
+        System.out.println("backstory");
+
+    }
+
+
+    // game goes by week, gives player the option on what to do, random events first
+    public void mainMenu() {
+        System.out.println("$money -- #plots -- #trees\nbuy trees\nbuy plots\nend week");
+        String userInput = repeatUntil(new String[]{"trees", "plots", "end week"});
+
+    }
+
+
 
     public void newTrees() {
         System.out.print("What kind of tree would you like?: ");
@@ -23,6 +41,7 @@ public class GameIO {
         String input = scan.nextLine();
         while (!stringInArray(strings, input)) {
             input = scan.nextLine();
+            System.out.print("Error please try again: ");
         }
 
         return input;
