5378f829d1bce588610d47e13a05b1339871f70c
BT_1N3_07 <sample@bths.edu>
2023-11-17 10:23:45 -0500

    added lpw variable

--- src/Tree.java
+++ src/Tree.java
@@ -1,25 +1,38 @@
 import java.util.List;
 
 public class Tree {
-    private char treeType;
+    private String treeType;
     private int treeSize;
+    private int treeProduction;
     // constructor for the Tree Class
-    public Tree(char treeType) {
+    public Tree(String treeType) {
         this.treeType = treeType;
         switch (treeType) {
-            case 'l' -> {
+            case "large" -> {
                 treeSize = 14;
+                treeProduction = 45;
             }
-            case 'm' -> {
+            case "medium" -> {
                 treeSize = 10;
+                treeProduction = 20;
             }
-            case 's' -> {
-                treeSize = 1;
+            case "small" -> {
+                treeSize = 7;
+                treeProduction = 5;
             }
+            default -> throw new IllegalStateException("Unexpected value: " + treeType);
         }
     }
 
     public int getTreeSize() {
         return treeSize;
+    }
+
+    public String getTreeType() {
+        return treeType;
+    }
+
+    public int lemonPerWeek() {
+        return treeProduction;
     }
 }
