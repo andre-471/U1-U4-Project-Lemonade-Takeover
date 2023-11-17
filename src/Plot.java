2faba06def984338aace416a9e607048c4923dd8
BT_1N3_07 <sample@bths.edu>
2023-11-17 10:04:15 -0500

    new methods involving tree. removed counter in favor of iterating thru list.

--- src/Plot.java
+++ src/Plot.java
@@ -4,15 +4,20 @@ public class Plot {
     private final int PLOTSIZE = 70;
     private ArrayList<Tree> trees;
     private int availableSpace;
-    private int smallTrees;
-    private int mediumTrees;
-    private int largeTrees;
-
     public Plot() {
         trees = new ArrayList<Tree>();
         availableSpace = PLOTSIZE;
     }
 
+    public int totalLemonPerWeek() {
+        int lemonPerWeek = 0;
+
+        for (Tree tree : trees) {
+            lemonPerWeek += tree.lemonPerWeek();
+        }
+
+        return lemonPerWeek;
+    }
     public void addTree(char treeSize) {
         Tree newTree = new Tree(treeSize);
         if (availableSpace - newTree.getTreeSize() > 0) {
@@ -20,9 +25,13 @@ public class Plot {
         }
     }
 
-    public addAndReplaceTree()
-
-    public void sellTree() {
+    public void addAndReplaceTree() {
 
     }
+
+    public void sellTree(int idx) {
+        char treeType = trees.get(idx).getTreeType();
+    }
+
+    private void decrement
 }
