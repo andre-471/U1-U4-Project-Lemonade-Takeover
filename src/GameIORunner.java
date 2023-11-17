0c6514f0e67ac07f8c7b12d8974f377b3d289bf5
BT_1N3_07 <sample@bths.edu>
2023-11-17 09:46:20 -0500

    removed runner

--- src/GameIORunner.java
+++ src/GameIORunner.java
@@ -1,6 +1,5 @@
 public class GameIORunner {
     public static void main(String[] args) {
         GameIO game = new GameIO();
-        game.newTrees();
     }
 }
--- src/GameRunner.java
+++
@@ -1,5 +0,0 @@
-public class GameRunner {
-    public static void main(String[] args) {
-        Game game = new Game();
-    }
-}
